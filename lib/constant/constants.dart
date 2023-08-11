3ec97ce4c3c08854364a06d09394adda95249329
reemashaji <reemashajiofficial@gmail.com>
2023-08-11 09:58:56 +0530

    Initial commit

---
+++ lib/constant/constants.dart
@@ -0,0 +1,17 @@
+String mapBoxApiKey =
+    'pk.eyJ1Ijoic2F1cmFiaG13IiwiYSI6ImNreTRiYzNidjBhMTkydnB2dmpoeGt4ZmgifQ.2QZ4CsNiygDTAhkqASpbPg';
+
+String nftStorageApiKey =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDEzMThDNDY4OUE5ZDNCMzFiOWFmNjExOTlkMUE1ZjBmMDZENTQzOTkiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTY4MTIyMzM2NzY4OCwibmFtZSI6ImxhbmRfcmVnaXN0cnkifQ.z2zcG0wbjekONzmTRXDb_Rc106PikHT675kGeFBPnCo';
+
+const String rpcUrl = "http://127.0.0.1:7545";
+// const String rpcUrl =
+//     "https://sepolia.infura.io/v3/acfb285cb753419c99eaf147cf3cbe40";
+const String contractAddress = "0x832636F1785937aA4D5977E627389CDf8154099c";
+//land_registry-const String contractAddress = "0xe53978218E1Af8c2Db9a1e136eF947e2B1f5d308";
+//const String contractAddress = "0x9caBB5546619B553A5C1A37f7F32281219126500";
+
+const int chainId = 1337; 
+//Polygon testnet-80001, local ganache-1337
+
+//const int chainId = 11155111;
